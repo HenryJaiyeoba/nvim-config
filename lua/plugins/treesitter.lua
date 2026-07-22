db6ddc1fb91664ad39a19cec02d3a88d3c3fecce
henryjaiyeoba <henryjaiyeoba@gmail.com>
2026-07-22 13:01:22 +0300

    Add autotag support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,12 +2,17 @@ return {
   'nvim-treesitter/nvim-treesitter',
   branch = 'master',
   build = ':TSUpdate',
+  dependencies = {
+    'windwp/nvim-ts-autotag',
+  },
   config = function()
     local configs = require 'nvim-treesitter.configs'
     configs.setup {
-      ensure_installed = { 'c', 'cpp', 'lua', 'vim', 'vimdoc', 'query', 'python' },
+      ensure_installed = { 'c', 'cpp', 'html', 'javascript', 'lua', 'python', 'query', 'tsx', 'typescript', 'vim', 'vimdoc' },
       highlight = { enable = true },
       indent = { enable = true },
     }
+
+    require('nvim-ts-autotag').setup()
   end,
 }
